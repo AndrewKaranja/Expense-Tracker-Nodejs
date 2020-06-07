fd0c7ec0102cef9ac7b533cdd024f2724d40515f
AndrewKaranja <andywanji56@gmail.com>
2020-06-07 23:37:37 +0300

    connection to database

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const app=express();
 
 app.use(express.json());
 
+if (process.env.NODE_ENV==='development') {
+   app.use(morgan('dev')); 
+}
+
 app.use('/api/v1/transactions',transactions);
 
 const PORT=process.env.PORT || 5000;
